f488dc145ba9c48d3fa80e66eb79df9c17f12959
musicode <musicode@qq.com>
2019-04-13 22:28:29 +0800

    update

--- test/parser.test.ts
+++ test/parser.test.ts
@@ -419,11 +419,11 @@ it('文本换行', () => {
 it('自动 bind', () => {
 
   let ast = compile(`
-    <div id="{{id}}" class="2"></div>
+    <div id="{{id}}" class="{{a.b.c}}" name="{{a + b}}" title="1"></div>
   `)
 
   expect(ast.length).toBe(1)
-  expect(ast[0].attrs.length).toBe(2)
+  expect(ast[0].attrs.length).toBe(4)
   expect(ast[0].children).toBe(undefined)
 
   expect(ast[0].attrs[0].type).toBe(nodeType.DIRECTIVE)
@@ -432,9 +432,18 @@ it('自动 bind', () => {
   expect(ast[0].attrs[0].expr).toBe(undefined)
   expect(ast[0].attrs[0].value).toBe('id')
 
-  expect(ast[0].attrs[1].type).toBe(nodeType.PROPERTY)
-  expect(ast[0].attrs[1].name).toBe('className')
-  expect(ast[0].attrs[1].value).toBe('2')
+  expect(ast[0].attrs[1].type).toBe(nodeType.DIRECTIVE)
+  expect(ast[0].attrs[1].name).toBe(config.DIRECTIVE_BIND)
+  expect(ast[0].attrs[1].modifier).toBe('className')
+  expect(ast[0].attrs[1].expr).toBe(undefined)
+  expect(ast[0].attrs[1].value).toBe('a.b.c')
+
+  expect(ast[0].attrs[2].type).toBe(nodeType.PROPERTY)
+  expect(ast[0].attrs[2].name).toBe('name')
+
+  expect(ast[0].attrs[3].type).toBe(nodeType.PROPERTY)
+  expect(ast[0].attrs[3].name).toBe('title')
+  expect(ast[0].attrs[3].value).toBe('1')
 
 })
 
