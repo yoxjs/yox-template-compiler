9fab0634a0ac937786b6cd1f6951e1ab792be997
musicode <musicode@qq.com>
2019-06-15 20:42:08 +0800

    update

--- src/compiler.ts
+++ src/compiler.ts
@@ -1,19 +1,19 @@
-import * as config from '../../yox-config/src/config'
+import * as config from 'yox-config/src/config'
 
-import toNumber from '../../yox-common/src/function/toNumber'
+import toNumber from 'yox-common/src/function/toNumber'
 
-import * as is from '../../yox-common/src/util/is'
-import * as env from '../../yox-common/src/util/env'
-import * as array from '../../yox-common/src/util/array'
-import * as string from '../../yox-common/src/util/string'
-import * as logger from '../../yox-common/src/util/logger'
+import * as is from 'yox-common/src/util/is'
+import * as env from 'yox-common/src/util/env'
+import * as array from 'yox-common/src/util/array'
+import * as string from 'yox-common/src/util/string'
+import * as logger from 'yox-common/src/util/logger'
 
-import * as exprNodeType from '../../yox-expression-compiler/src/nodeType'
-import * as exprCompiler from '../../yox-expression-compiler/src/compiler'
+import * as exprNodeType from 'yox-expression-compiler/src/nodeType'
+import * as exprCompiler from 'yox-expression-compiler/src/compiler'
 
-import ExpressionNode from '../../yox-expression-compiler/src/node/Node'
-import ExpressionCall from '../../yox-expression-compiler/src/node/Call'
-import ExpressionLiteral from '../../yox-expression-compiler/src/node/Literal'
+import ExpressionNode from 'yox-expression-compiler/src/node/Node'
+import ExpressionCall from 'yox-expression-compiler/src/node/Call'
+import ExpressionLiteral from 'yox-expression-compiler/src/node/Literal'
 
 import * as helper from './helper'
 import * as creator from './creator'
--- src/renderer.ts
+++ src/renderer.ts
@@ -1,27 +1,27 @@
-import * as config from '../../yox-config/src/config'
-import * as type from '../../yox-type/src/type'
+import * as config from 'yox-config/src/config'
+import * as type from 'yox-type/src/type'
 
-import isDef from '../../yox-common/src/function/isDef'
-import isUndef from '../../yox-common/src/function/isUndef'
-import execute from '../../yox-common/src/function/execute'
-import toString from '../../yox-common/src/function/toString'
+import isDef from 'yox-common/src/function/isDef'
+import isUndef from 'yox-common/src/function/isUndef'
+import execute from 'yox-common/src/function/execute'
+import toString from 'yox-common/src/function/toString'
 
-import * as is from '../../yox-common/src/util/is'
-import * as env from '../../yox-common/src/util/env'
-import * as array from '../../yox-common/src/util/array'
-import * as object from '../../yox-common/src/util/object'
-import * as logger from '../../yox-common/src/util/logger'
-import * as keypathUtil from '../../yox-common/src/util/keypath'
+import * as is from 'yox-common/src/util/is'
+import * as env from 'yox-common/src/util/env'
+import * as array from 'yox-common/src/util/array'
+import * as object from 'yox-common/src/util/object'
+import * as logger from 'yox-common/src/util/logger'
+import * as keypathUtil from 'yox-common/src/util/keypath'
 
-import valueHolder from '../../yox-common/src/util/valueHolder'
+import valueHolder from 'yox-common/src/util/valueHolder'
 
-import CustomEvent from '../../yox-common/src/util/CustomEvent'
+import CustomEvent from 'yox-common/src/util/CustomEvent'
 
-import Yox from '../../yox-type/src/interface/Yox'
-import VNode from '../../yox-type/src/vnode/VNode'
-import DirectiveHooks from '../../yox-type/src/hooks/Directive'
-import TransitionHooks from '../../yox-type/src/hooks/Transition'
-import ValueHolder from '../../yox-type/src/interface/ValueHolder'
+import Yox from 'yox-type/src/interface/Yox'
+import VNode from 'yox-type/src/vnode/VNode'
+import DirectiveHooks from 'yox-type/src/hooks/Directive'
+import TransitionHooks from 'yox-type/src/hooks/Transition'
+import ValueHolder from 'yox-type/src/interface/ValueHolder'
 
 function setPair(target: any, name: string, key: string, value: any) {
   const data = target[name] || (target[name] = {})
