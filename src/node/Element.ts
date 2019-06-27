bdf519c63af3cbe05b8c58e8b51ea020a8ace1ab
musicode <musicode@qq.com>
2019-06-27 16:33:04 +0800

    update

--- src/node/Element.ts
+++ src/node/Element.ts
@@ -13,12 +13,17 @@ export default interface Element extends Branch {
 
   tag: string
 
+  // 是否是 svg 类族元素
   isSvg: boolean
 
+  // 是否是 <style> 元素
   isStyle: boolean
 
+  // 是否是 <option> 元素
   isOption: boolean
 
+  // 是否是组件元素，相对 DOM 元素来的
+  // 如果未来要搞跨平台方案，此属性可忽略
   isComponent: boolean
 
   // <template slot="xx">
